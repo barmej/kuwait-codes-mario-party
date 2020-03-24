bd666dbf6714d73d6a597b4e82a4431157dbc9f1
Omar <omsi96@users.noreply.github.com>
2020-03-24 16:39:30 +0300

    Mario Party uploaded

---
+++ MarioParty/MarioParty/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  MarioParty
+//
+//  Created by Dalal Alhazeem on 3/24/20.
+//  Copyright © 2020 Dalal Alhazeem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+   
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        //nextButton.isUserInteractionEnabled = false
+    }
+
+
+}
+
